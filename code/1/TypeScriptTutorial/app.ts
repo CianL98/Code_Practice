3ca39f1d97aad745e573103dffc8098faa14a50d
A00240928 <34064542+A00240928@users.noreply.github.com>
2020-09-30 14:45:52 +0100

    type aliases

--- code/1/TypeScriptTutorial/app.ts
+++ code/1/TypeScriptTutorial/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionD = 'as-number' | 'as-text';
+
 function combine(
-    input1: number | string, 
-    input2: number | string, 
-    resultConverion: 'as-number' | 'as-text'
+    input1: Combinable, 
+    input2: Combinable, 
+    resultConverion: ConversionD
     ){
     let result;
     if(typeof input1 === 'number' && typeof input2 === 'number' || resultConverion === 'as-number'){
