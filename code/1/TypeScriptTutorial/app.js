0a08231bbde7ddbf36f7beab8f70074cf8f5ad8f
A00240928 <34064542+A00240928@users.noreply.github.com>
2020-09-30 14:10:44 +0100

    tuples

--- code/1/TypeScriptTutorial/app.js
+++ code/1/TypeScriptTutorial/app.js
@@ -5,12 +5,17 @@
 var person = {
     name: 'Cian',
     age: 21,
-    hobbies: ['Coding', 'Cooking']
+    hobbies: ['Coding', 'Cooking'],
+    role: [2, 'Author']
 };
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 var favouriteActivities;
 favouriteActivities = ['coding'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); // !!!ERROR!!!
 }
--- code/1/TypeScriptTutorial/app.ts
+++ code/1/TypeScriptTutorial/app.ts
@@ -2,11 +2,21 @@
 //    name: string;
 //    age: number;
 //} = {
-    const person = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number,string];
+    } = {
     name: 'Cian',
     age: 21,
-    hobbies: ['Coding', 'Cooking']
+    hobbies: ['Coding', 'Cooking'],
+    role: [2, 'Author']
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 
 let favouriteActivities: string[];
 favouriteActivities = ['coding'];
