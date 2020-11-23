f48fe9a5f931dbf9679d90d5e76cc74288464246
Cian Lennon <cianlennon98@gmail.com>
2020-11-23 14:03:04 +0000

    setting up tooling

--- code/1/building-web-applications-using-nodejs-and-express/code/library/app.js
+++ code/1/building-web-applications-using-nodejs-and-express/code/library/app.js
@@ -1,21 +1,22 @@
-var express = require('express');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
-var port = 5000;
- 
-app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
-app.get('/books', function(req, res){
-    res.send('Hello Books');
-});
-
-app.listen(port, function(err){
-    console.log('running server on port ' + port);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
