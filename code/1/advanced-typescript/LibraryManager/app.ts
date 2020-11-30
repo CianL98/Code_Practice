8fdbd52c316c518330b18a90a745e75ba8830ebd
Cian Lennon <cianlennon98@gmail.com>
2020-11-30 12:55:32 +0000

    tslint

--- code/1/advanced-typescript/LibraryManager/app.ts
+++ code/1/advanced-typescript/LibraryManager/app.ts
@@ -3,89 +3,21 @@ import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
 import { PublicLibrarian, UniversityLibrarian, ReferenceItem, Employee, Researcher } from './classes';
 import * as util from './lib/utilityFunctions';
 
-//**********************************************************
-// Callbacks demo
-
-// interface LibMgrCallback {
-// 	(err: Error, titles: string[]): void;
-// }
-
-// function getBooksByCategory(cat: Category, callback: LibMgrCallback): void {
-
-// 	setTimeout(() => {
-// 		try {
-// 			let foundBooks: string[] = util.GetBookTitlesByCategory(cat);
-
-// 			if(foundBooks.length > 0) {
-// 				callback(null, foundBooks);
-// 			}
-// 			else {
-// 				throw new Error('No books found.');
-// 			}			
-// 		} catch (error) {
-// 			callback(error, null);
-// 		}
-// 	}, 2000);
-// }
-
-// function logCategorySearch(err: Error, titles: string[]): void {
-// 	if(err) {
-// 		console.log(`Error message: ${err.message}`);
-// 	}
-// 	else {
-// 		console.log(`Found the following titles:`);
-// 		console.log(titles);
-// 	}	
-// }
-
-//**********************************************************
-// Promises demo
-
-// console.log('Beginning search...');
-// getBooksByCategory(Category.Biography, logCategorySearch);
-// console.log('Search submitted...');
-
-
-// function getBooksByCategory(cat: Category): Promise<string[]> {
-
-// 	let p: Promise<string[]> = new Promise((resolve, reject) => {
-
-// 		setTimeout(() => {
-// 			let foundBooks: string[] = util.GetBookTitlesByCategory(cat);
-
-// 			if(foundBooks.length > 0) {
-// 				resolve(foundBooks);
-// 			}
-// 			else {
-// 				reject('No books found for that category.');
-// 			}
-// 		}, 2000);
-// 	});
-// 	return p;
-// }
-
-// console.log('Beginning search...');
-// getBooksByCategory(Category.Fiction)
-// 	.then(titles => {
-// 		console.log(`Found titles: ${titles}`);
-// 		throw 'something bad happened';
-// 		return titles.length;
-// 	}, reason => { return 0; })
-// 	.then(numOfBooks => console.log(`Number of books found: ${numOfBooks}`))
-// 	.catch(reason => console.log(`Error: ${reason}`));
-// console.log('Search submitted...');
-
-//**********************************************************
-// async/await demo
-
+// demo code from "Implementing Asynchronous Patters" module
+// show how to use async/await
 function getBooksByCategory(cat: Category): Promise<string[]> {
+
+	console.log('got a semi-colon?');
+
+	console.log('tabs or spaces?');
+
 
 	let p: Promise<string[]> = new Promise((resolve, reject) => {
 
 		setTimeout(() => {
 			let foundBooks: string[] = util.GetBookTitlesByCategory(cat);
 
-			if(foundBooks.length > 0) {
+			if (foundBooks.length > 0) {
 				resolve(foundBooks);
 			}
 			else {
